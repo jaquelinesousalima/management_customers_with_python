8609afd5ef53a7cf67fb515a6ca3fcc33280528e
Jaqueline Sousa Lima <jaquelinesousalima@outlook.com>
2026-03-18 17:38:00 +0000

    Commit v0

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("New Customer")
+
+name = st.text_input("Type the name of customer")
+address = st.text_input("Type the address")
+birth_date = st.date_input("Choose the birth date")
+type_of_customer = st.selectbox("Type of customer", ["Individual", "Legal Entity"])
+
+save = st.button("Save")
+
+if save:
+    with open("customers_data_base.csv", "a", encoding="utf-8") as file:
+        file.write(f"{name},{address},{birth_date},{type_of_customer}\n")
+        st.success("Customer saved successfully!")
